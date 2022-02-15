421db4c93c9d783392a28f0040a30679e5162d59
ali-ch88 <raza7861991@gmail.com>
2022-02-15 23:00:06 +0500

    form auth

--- src/components/Profile/UserProfile.js
+++ src/components/Profile/UserProfile.js
@@ -2,6 +2,32 @@ import ProfileForm from './ProfileForm';
 import classes from './UserProfile.module.css';
 
 const UserProfile = () => {
+
+  const authHandler = () => {
+    headers: {
+      'Content-Type' : 'application/Json'
+    }
+  }).then(res => {
+    setIsLoading(false);
+    if (res.ok) {
+       return res.json();
+    } else {
+      return res.json().then(data => {
+        let errorMessage  = 'Authentication Error';
+        if (data && data.error && data.error.message) {
+          errorMessage = data.error.message;
+        }
+        alert(errorMessage);
+        throw new Error(errorMessage);
+       
+      });
+    }
+  }).then(data => {
+    authCts.login(data.idToken);
+  }).catch((err) => {
+    alert(err.message);
+  });
+  }
   return (
     <section className={classes.profile}>
       <h1>Your User Profile</h1>
